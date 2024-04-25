27ae97f29b67592c6a220a21c7457c2d86115f86
vishwasdev01 <vishwasdev01@gmail.com>
2024-04-25 18:08:25 +0530

    Added logics for transactions request.

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,2 +1,54 @@
 class TransactionsController < ApplicationController
-end
+    before_action :set_user
+    before_action :set_transaction, only: [:show, :edit, :update, :destroy]
+  
+    def index
+      @transactions = @user.transactions
+    end
+  
+    def show
+    end
+  
+    def new
+      @transaction = @user.transactions.new
+    end
+  
+    def edit
+    end
+  
+    def create
+      @transaction = @user.transactions.new(transaction_params)
+      if @transaction.save
+        redirect_to user_transactions_path(@user), notice: 'Transaction was successfully created.'
+      else
+        render :new
+      end
+    end
+  
+    def update
+      if @transaction.update(transaction_params)
+        redirect_to user_transactions_path(@user), notice: 'Transaction was successfully updated.'
+      else
+        render :edit
+      end
+    end
+  
+    def destroy
+      @transaction.destroy
+      redirect_to user_transactions_path(@user), notice: 'Transaction was successfully destroyed.'
+    end
+  
+    private
+      def set_user
+        @user = User.find(params[:user_id])
+      end
+  
+      def set_transaction
+        @transaction = @user.transactions.find(params[:id])
+      end
+  
+      def transaction_params
+        params.require(:transaction).permit(:amount, :user_id, :country)
+      end
+  end
+  
